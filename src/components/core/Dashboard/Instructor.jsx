ce0dfdd8eb1d67016811175cfea9a41e2337d0a3
LakshyaAhlawat <ahlawat.lakshya.2004@gmail.com>
2026-01-22 19:38:58 +0530

    chore: fix ESLint warnings for CI build

--- src/components/core/Dashboard/Instructor.jsx
+++ src/components/core/Dashboard/Instructor.jsx
@@ -198,7 +198,7 @@ export default function Instructor() {
       }
       setLoading(false)
     })()
-  }, [])
+  }, [token])
 
   const totalAmount = instructorData?.reduce(
     (acc, curr) => acc + curr.totalAmountGenerated,
